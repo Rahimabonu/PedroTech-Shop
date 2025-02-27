8fa18c25d53455e9d650a541d550b03a7727500e
javlonbek1010 <javlonbek.001.200@gmail.com>
2025-02-27 16:15:18 +0500

    css qoshdim

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -6,7 +6,8 @@ const Product = ({imgURL,name,narx,id}) => {
         <div className="product">
             <img className='product-img' src={imgURL} alt="" />
             <div className="product-name">{name}</div>
-            <div className="product-narx">{narx}</div>
+            <div className="product-name">{narx}</div>
+            <button className='product-btn'>Add to Cart</button>
         </div>
     </>
 )
